e0221566d7a744f6b3d44a076e0881723da88da8
Giovan-pemula <Giovanfernandes82@gmail.com>
2026-04-06 21:51:21 +0700

    fix: change seed.ts to suit nest framework

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,21 @@
-import prisma from "../src/config/database";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/common/prisma/prisma.service';
 
 async function main() {
-  console.log("Seeding database...");
+  const app = await NestFactory.createApplicationContext(AppModule);
+
+  const prisma = app.get(PrismaService);
+
+  console.log('Seeding database...');
+
+//seed dibawah sini
+
+  await prisma.$disconnect();
+  await app.close();
 }
 
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
